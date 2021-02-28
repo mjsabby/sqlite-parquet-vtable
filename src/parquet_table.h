9d279a46a4021a99f970ca21065138c73687eeb0
Mukul Sabharwal <mjsabby@gmail.com>
2021-02-28 12:48:30 -0800

    Apply Microsoft style guide using clang-format

--- src/parquet_table.h
+++ src/parquet_table.h
@@ -1,25 +1,25 @@
 #ifndef PARQUET_TABLE_H
 #define PARQUET_TABLE_H
 
-#include <vector>
-#include <string>
 #include "parquet/api/reader.h"
+#include <string>
+#include <vector>
 
-class ParquetTable {
-  std::string file;
-  std::string tableName;
-  std::vector<std::string> columnNames;
-  std::shared_ptr<parquet::FileMetaData> metadata;
+class ParquetTable
+{
+    std::string file;
+    std::string tableName;
+    std::vector<std::string> columnNames;
+    std::shared_ptr<parquet::FileMetaData> metadata;
 
-
-public:
-  ParquetTable(std::string file, std::string tableName);
-  std::string CreateStatement();
-  std::string columnName(int idx);
-  unsigned int getNumColumns();
-  std::shared_ptr<parquet::FileMetaData> getMetadata();
-  const std::string& getFile();
-  const std::string& getTableName();
+  public:
+    ParquetTable(std::string file, std::string tableName);
+    std::string CreateStatement();
+    std::string columnName(int idx);
+    unsigned int getNumColumns();
+    std::shared_ptr<parquet::FileMetaData> getMetadata();
+    const std::string &getFile();
+    const std::string &getTableName();
 };
 
 #endif
